56e75f1a2e3a50861daaa32318314511df92225b
Raul <krolik-0304@bk.ru>
2023-03-02 17:36:40 +0600

    Initial Commit

---
+++ W2M3/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  W2M3
+//
+//  Created by Рауль on 2/3/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
